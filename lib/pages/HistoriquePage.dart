70035aceb68a9fbdb0af29350d3d49dfa656f37b
mamour fall <mamourf958@gmail.com>
2024-06-27 21:25:28 +0000

    modif

---
+++ lib/pages/HistoriquePage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HistoriquePage extends StatefulWidget {
+  const HistoriquePage({super.key});
+
+  @override
+  State<HistoriquePage> createState() => _HistoriquePageState();
+}
+
+class _HistoriquePageState extends State<HistoriquePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
