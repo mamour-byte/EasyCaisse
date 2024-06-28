5b769ebe1b9ff7bd5a6ac227c0d75c89c86cf2db
mamour fall <mamourf958@gmail.com>
2024-06-28 10:40:16 +0000

    modif

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
-class HomePgae extends StatefulWidget {
-  const HomePgae({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<HomePgae> createState() => _HomePgaeState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePgaeState extends State<HomePgae> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+
   }
 }
